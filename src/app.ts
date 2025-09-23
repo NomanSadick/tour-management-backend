17eaf71518747c044ef329b360de451232a7a21c
Noman Sadick <nomansadickjs@gmail.com>
2025-09-23 17:40:06 +0600

    part-3 complete

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,26 @@
 import cors from "cors";
-import express, { NextFunction, Request, Response } from "express";
+import express, { Request, Response } from "express";
 import { router } from "./app/routes";
-import { envVars } from "./app/config/env";
 import { globalErrorHandler } from "./app/middlewares/globalErrorHandler";
 import notFound from "./app/middlewares/notFound";
+import cookieParser from "cookie-parser";
+import passport from "passport";
+import expressSession from "express-session";
+import "./app/config/passport";
 
 
 
 
 const app = express();
+app.use(expressSession({
+  secret: "secret",
+  resave: false,
+  saveUninitialized: false
+}))
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(cookieParser());
 
 app.use(express.json());
 app.use(cors());
--- src/app/modules/auth/auth.service.ts
+++ src/app/modules/auth/auth.service.ts
@@ -1,41 +1,78 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
 import AppError from "../../errorHelpers/AppError";
 import { IUser } from "../user/user.interface";
 import { User } from "../user/user.model";
 import httpStatus from "http-status-codes";
 import bcryptjs from "bcryptjs";
-import jwt from "jsonwebtoken";
-import { generateToken } from "../../utils/jwt";
+import { createNewAccessTokenWithRefreshToken, createUserTokens } from "../../utils/userTokens";
+import { JwtPayload } from "jsonwebtoken";
 import { envVars } from "../../config/env";
+
 
 const credentialsLogin = async (payload: Partial<IUser>) => {
   const { email, password } = payload;
 
   const isUserExist = await User.findOne({ email });
 
-
   if (!isUserExist) {
-    throw new AppError("Email already exists", httpStatus.BAD_REQUEST, "");
+    throw new AppError(httpStatus.BAD_REQUEST, "Email already exists");
   }
 
-  const isPasswordMatch = await bcryptjs.compare(password as string, isUserExist.password as string);
-    if (!isPasswordMatch) {
-      throw new AppError("Password is incorrect", httpStatus.UNAUTHORIZED, "");
-    }
+  const isPasswordMatch = await bcryptjs.compare(
+    password as string,
+    isUserExist.password as string
+  );
+  if (!isPasswordMatch) {
+    throw new AppError(httpStatus.UNAUTHORIZED, "Password is incorrect");
+  }
 
-    const jwtPayload = {
-        userId: isUserExist._id,
-        email: isUserExist.email,
-        role: isUserExist.role
-    }
+  // const jwtPayload = {
+  //     userId: isUserExist._id,
+  //     email: isUserExist.email,
+  //     role: isUserExist.role
+  // }
 
-    const accessToken = generateToken(jwtPayload, envVars.JWT_ACCESS_SECRET, envVars.JWT_ACCESS_EXPIRE);
-    // const accessToken = jwt.sign(jwtPayload, "secret", { expiresIn: "1d" });
+  // const accessToken = generateToken(jwtPayload, envVars.JWT_ACCESS_SECRET, envVars.JWT_ACCESS_EXPIRE);
+  // const refreshToken = generateToken(jwtPayload, envVars.JWT_REFRESH_SECRET, envVars.JWT_REFRESH_EXPIRE);
 
-    return {
-        accessToken
-    }
+  const userTokens = createUserTokens(isUserExist);
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const { password: pass, ...rest } = isUserExist.toObject();
+
+  return {
+    accessToken: userTokens.accessToken,
+    refreshToken: userTokens.refreshToken,
+    user: rest,
+  };
+};
+
+const getNewAccessToken = async (refreshToken: string) => {
+
+  const newAccessToken = await createNewAccessTokenWithRefreshToken(refreshToken);
+  
+
+  return {
+    accessToken : newAccessToken,
+  };
+};
+
+
+const resetPassword = async (oldPassword: string, newPassword: string, decodedToken: JwtPayload) => {
+  const user = await User.findById(decodedToken.userId);
+
+  const isOldPasswordMatch = await bcryptjs.compare(oldPassword, user!.password as string);
+  if (!isOldPasswordMatch) {
+    throw new AppError(httpStatus.UNAUTHORIZED, "Old password is incorrect");
+  }
+
+  user!.password = await bcryptjs.hash(newPassword, Number(envVars.BCRYPT_SALT_ROUNDS));
+  user!.save();
+ 
+
 };
 
 export const AuthService = {
   credentialsLogin,
+  getNewAccessToken,
+  resetPassword,
 };
